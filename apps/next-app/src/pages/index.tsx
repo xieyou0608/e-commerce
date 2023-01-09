c64df1535274cccde9e46926a5a1f79bad29fc36
xieyou0608 <b07705003@ntu.edu.tw>
2023-01-10 00:52:48 +0800

    fix: landing page RWD

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -3,17 +3,17 @@ import Image from 'next/image';
 export function Index() {
   return (
     <main className="w-full flex flex-col items-center">
-      <div className="flex gap-x-5 text-2xl font-bold text-gray-400 my-5">
+      <div className="flex flex-wrap gap-x-5 text-2xl font-bold text-gray-400 my-5 ml-[10vw] lg:ml-0">
         <Link href="/products">All</Link>|<Link href="/products">Women</Link>|
         <Link href="/products">Men</Link>|<Link href="/products">Jewelery</Link>
         |<Link href="/products">electronic</Link>
       </div>
-      <div className="w-[90vw] h-[100vh] relative">
+      <div className="w-[90vw] h-[50vh] lg:h-[100vh] relative">
         <Image
           src="https://i.imgur.com/2eFUhHk.jpg"
           alt="landing-page-image"
           fill
-          style={{ objectFit: 'contain' }}
+          className="object-contain"
           priority
         />
       </div>
